057193a9c9d0c0e44d75a7389eeccac3ff55ee8a
cperry-adv <cperry@advanstar.com>
2017-10-14 22:11:49 -0700

    ya

--- sites/all/modules/fancy/js/js.js
+++ sites/all/modules/fancy/js/js.js
@@ -1,28 +1,48 @@
 jQuery(document).ready(function() {
 
-	function initProgressBar() {
+	function calculateTotalValue(length) {
+  var minutes = Math.floor(length / 60),
+    seconds_int = length - minutes * 60,
+    seconds_str = seconds_int.toString(),
+    seconds = seconds_str.substr(0, 2),
+    time = minutes + ':' + seconds
+
+  return time;
+}
+
+function calculateCurrentValue(currentTime) {
+  var current_hour = parseInt(currentTime / 3600) % 24,
+    current_minute = parseInt(currentTime / 60) % 60,
+    current_seconds_long = currentTime % 60,
+    current_seconds = current_seconds_long.toFixed(),
+    current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+
+  return current_time;
+}
+
+function initProgressBar() {
   var player = document.getElementById('player');
   var length = player.duration
   var current_time = player.currentTime;
 
   // calculate total length of value
   var totalLength = calculateTotalValue(length)
-  document.getElementById("end-time").innerHTML = totalLength;
+  jQuery(".end-time").html(totalLength);
 
   // calculate current value time
   var currentTime = calculateCurrentValue(current_time);
-  document.getElementById("start-time").innerHTML = currentTime;
+  jQuery(".start-time").html(currentTime);
 
-  var progressbar = document.getElementById('seek-obj');
+  var progressbar = document.getElementById('seekObj');
   progressbar.value = (player.currentTime / player.duration);
   progressbar.addEventListener("click", seek);
 
   if (player.currentTime == player.duration) {
-    document.getElementById('play-btn').className = "";
+    $('#play-btn').removeClass('pause');
   }
 
-  function seek(event) {
-    var percent = event.offsetX / this.offsetWidth;
+  function seek(evt) {
+    var percent = evt.offsetX / this.offsetWidth;
     player.currentTime = percent * player.duration;
     progressbar.value = percent / 100;
   }
@@ -56,36 +76,16 @@ function initPlayers(num) {
         if (player.paused === false) {
           player.pause();
           isPlaying = false;
-          document.getElementById('play-btn').className = "";
+          $('#play-btn').removeClass('pause');
 
         } else {
           player.play();
-          document.getElementById('play-btn').className = "pause";
+          $('#play-btn').addClass('pause');
           isPlaying = true;
         }
       }
     }());
   }
-}
-
-function calculateTotalValue(length) {
-  var minutes = Math.floor(length / 60),
-    seconds_int = length - minutes * 60,
-    seconds_str = seconds_int.toString(),
-    seconds = seconds_str.substr(0, 2),
-    time = minutes + ':' + seconds
-
-  return time;
-}
-
-function calculateCurrentValue(currentTime) {
-  var current_hour = parseInt(currentTime / 3600) % 24,
-    current_minute = parseInt(currentTime / 60) % 60,
-    current_seconds_long = currentTime % 60,
-    current_seconds = current_seconds_long.toFixed(),
-    current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
-
-  return current_time;
 }
 
 initPlayers(jQuery('#player-container').length);
